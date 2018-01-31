31339100a545f9a2d0fdd6cee7c43acfd29a2862
Nik Muhammad Naim Nik Ghazali <niknaim24@gmail.com>
2018-02-01 02:57:49 +0800

    Make our data into xts object.

--- Analyzing Financial Data and Implementing Financial Models Using R/Chapter1.R
+++ Analyzing Financial Data and Implementing Financial Models Using R/Chapter1.R
@@ -2,5 +2,22 @@ library(tidyverse)
 library(tidyquant)
 
 # Import Amazon stock price data from Yahoo Finance from December 31, 2010 to December 31, 2013
-amazon <- tq_get("AMZN", get = "stock.prices", from = "2010-12-31", to = "2013-12-31")
+amazon <- tq_get("AMZN", get = "stock.prices", from = "2010-12-31", to = "2014-01-01")
 amazon
+
+# Renaming our columns
+amazon <- amazon %>% 
+  rename(AMZN.Open = open,
+         AMZN.High = high,
+         AMZN.Low = low,
+         AMZN.Close = close,
+         AMZN.Volume = volume, 
+         AMZN.Adjusted = adjusted)
+amazon
+
+# Convert our data into xts object
+amazon <- xts(amazon[,2:7], order.by = amazon$date)
+amazon %>% head(15)
+amazon %>% tail(15)
+class(amazon)
+
