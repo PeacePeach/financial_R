728f9e9b36753f954250c2bf81cc6370cdf2c321
Nik Muhammad Naim Nik Ghazali <niknaim24@gmail.com>
2018-02-02 01:41:47 +0800

    Completed until 1.4.11

--- Analyzing Financial Data and Implementing Financial Models Using R/Chapter1.R
+++ Analyzing Financial Data and Implementing Financial Models Using R/Chapter1.R
@@ -21,3 +21,52 @@ amazon %>% head(15)
 amazon %>% tail(15)
 class(amazon)
 
+# -------------------------
+# Checking the Data
+# -------------------------
+# Verify that the data is complete by plotting a chart
+plot(amazon$AMZN.Close)
+
+# And by Checking the summary
+summary(amazon)
+
+
+# -------------------------
+# Basic Data Manipulation
+# -------------------------
+# a) Subsetting using dates
+# --> Data is an xts object
+xts.2012 <- subset(amazon$AMZN.Close, index(amazon) >= "2012-01-01" & index(amazon) <= "2012-12-31")
+xts.2012[c(1:3, nrow(xts.2012)), ]
+
+#--> Data is a data.frame object
+# First we need to change our data into a dataframe
+amazon.2012 <- cbind(date = index(amazon), data.frame(amazon[, "AMZN.Close"]))
+amazon.2012[c(1:3, nrow(amazon.2012)), ]
+class(amazon.2012)
+
+# Change the index 
+rownames(amazon.2012) <- seq(1, nrow(amazon.2012))
+amazon.2012[c(1:3, nrow(amazon.2012)), ]
+
+# Subset the data
+amazon.2012 <- subset(amazon.2012, amazon.2012$date >= "2012-01-01" & amazon.2012$date <= "2012-12-31")
+amazon.2012[c(1:3, nrow(amazon.2012)), ]
+
+
+# b) Converting daily prices to weekly and monthly prices
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
